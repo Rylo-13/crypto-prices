e7183bad2de62dc3772e5f23553e1197e17c42a7
Rylan Mcneil <rylanmcneil@hotmail.com>
2023-01-03 02:12:39 +1100

    Started Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="header">
+      <div className="container">
+        <h1>
+          De<span className="primary">Fi</span>
+        </h1>
+        <ul className="nav-menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Featured</a>
+          </li>
+          <li>
+            <a href="/">Earn</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group"></div>
+        <button className="btn">Connect Wallet</button>
+        <div className="hamburger">
+          <FaBars size={20} style={{ color: "#333" }} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
